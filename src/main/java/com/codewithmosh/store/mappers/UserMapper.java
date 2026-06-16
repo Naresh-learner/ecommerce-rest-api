1d48037479308d0c7dc794a80b4ba9d1e71024b7
Naresh Yarlagadda <yarlagaddanaresh2000@gmail.com>
2026-06-16 18:02:19 -0400

    Add POST,PUT and Delete user endpoints with validation and global error handling

--- src/main/java/com/codewithmosh/store/mappers/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/UserMapper.java
@@ -1,13 +1,20 @@
 package com.codewithmosh.store.mappers;
 
 
+import com.codewithmosh.store.dtos.RegisterUserRequest;
+import com.codewithmosh.store.dtos.UpdateUserRequest;
 import com.codewithmosh.store.dtos.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")// So spring can create beans of this type at run time.
 public interface UserMapper {
 // @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
+
+    void update(UpdateUserRequest request, @MappingTarget User user);
+
+    User toEntity(RegisterUserRequest request);
 }
